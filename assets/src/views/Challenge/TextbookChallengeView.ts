e0571747712e1ce98c937e79a9e0594185fa4bbe
Ray <757964954@qq.com>
2024-04-28 11:58:19 +0800

    我得词书下方模块

--- assets/src/views/Challenge/TextbookChallengeView.ts
+++ assets/src/views/Challenge/TextbookChallengeView.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, error, instantiate, Node, Prefab, view } from 'cc';
+import { _decorator, Component, error, instantiate, isValid, Node, Prefab, view, Widget } from 'cc';
 import { PrefabType } from '../../config/PrefabType';
 import { ResLoader } from '../../manager/ResLoader';
 import { ViewsManager } from '../../manager/ViewsManager';
@@ -12,6 +12,8 @@ export class TextbookChallengeView extends Component {
     @property(Node)
     public top_layout:Node = null;          // 顶部导航栏
 
+    @property(Node)
+    public content_layout:Node = null;          // 内容
 
     private unitArr:any[] = [];
     private _unitDetailView:RightUnitView = null;
@@ -21,9 +23,9 @@ export class TextbookChallengeView extends Component {
     protected initUI(){
         this.initNavTitle();
         this.initAmout();
-        // this.initBottomAutoScroll();
-        // this.initChallengeBottom();
-        // this.initRightMonsterDetail();
+        this.initChallengeBottom();
+        this.initLeftMonster();
+        this.initRightBookUnitInfo();
     }
     /**初始化导航栏 */
     initNavTitle(){
@@ -43,33 +45,26 @@ export class TextbookChallengeView extends Component {
         });
     }
 
-    /**初始化自动滚动角色UI */
-    initBottomAutoScroll(){
-        ResLoader.instance.load(`prefab/${PrefabType.FloorsAutoView.path}`, Prefab, (err: Error | null, prefab: Prefab) => {
-            if (err) {
-                error && console.error(err);
-                return;
-            }
-            let node = instantiate(prefab);
-            this.node.addChild(node);
-        });
-    }
-
     /**出事右侧怪物详情 */
-    initRightMonsterDetail(){
+    initRightBookUnitInfo(){
         ResLoader.instance.load(`prefab/${PrefabType.RightUnitView.path}`, Prefab, (err: Error | null, prefab: Prefab) => {
             if (err) {
                 error && console.error(err);
                 return;
             }
             let node = instantiate(prefab);
-            this.node.addChild(node);
-            let winWidth = view.getVisibleSize().width;
-            node.setPosition(winWidth/2, 0, 0);
+            this.content_layout.addChild(node);
             this._unitDetailView = node.getComponent(RightUnitView);
+            let widgetCom = node.getComponent(Widget);
+            if(!isValid(widgetCom)){
+                widgetCom = node.addComponent(Widget);
+                widgetCom.isAlignRight = true;
+                widgetCom.isAlignVerticalCenter= true;
+            }
+            widgetCom.verticalCenter = 62.308;
+            widgetCom.right = 62.308;
         });
     }
-
     /**下方单元进度模块 */
     initChallengeBottom(){
         ResLoader.instance.load(`prefab/${PrefabType.ChallengeBottomView.path}`, Prefab, (err: Error | null, prefab: Prefab) => {
@@ -81,7 +76,32 @@ export class TextbookChallengeView extends Component {
             this.node.addChild(node);
         });
     }
-
+    /**初始化左侧怪物 */
+    initLeftMonster(){
+        ResLoader.instance.load(`prefab/${PrefabType.LeftMonsterView.path}`, Prefab, (err: Error | null, prefab: Prefab) => {
+            if (err) {
+                error && console.error(err);
+                return;
+            }
+            let node = instantiate(prefab);
+            this.content_layout.addChild(node);
+            let widgetCom = node.getComponent(Widget);
+            if(!isValid(widgetCom)){
+                widgetCom = node.addComponent(Widget);
+                widgetCom.isAlignLeft = true;
+                // widgetCom.isAlignTop= true;
+                widgetCom.isAlignVerticalCenter= true;
+            }
+            let viewSizeWidth = view.getVisibleSize().width;
+            let projectSizeWidth = view.getDesignResolutionSize().width;
+            console.log("viewSizeWidth = ", viewSizeWidth, " projectSizeWidth = ", projectSizeWidth);
+            widgetCom.verticalCenter = 78.489;
+            widgetCom.left = 179.221 * viewSizeWidth / projectSizeWidth;
+            widgetCom.updateAlignment();
+            // widgetCom.top = 97.93;
+            // widgetCom.left = 108.736;
+        });
+    }
     update(deltaTime: number) {
         
     }
