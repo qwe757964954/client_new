cdc4159a8f11731f71e4c9e585194e81c62bb247
Ray <757964954@qq.com>
2024-05-29 17:40:18 +0800

    结算页面动画

--- assets/src/views/Challenge/BreakThroughView.ts
+++ assets/src/views/Challenge/BreakThroughView.ts
@@ -139,6 +139,7 @@ export class BreakThroughView extends BaseView {
 
     onVocabularyWord(data:VocabularyWordData){
         console.log("onVocabularyWord", data);
+        this._curUnitStatus.game_mode = 4
         let game_model:LearnGameModel = this._curUnitStatus.game_mode as LearnGameModel;
         let bookLevelData:BookLevelConfig = {
             id:this._bookData.id,
