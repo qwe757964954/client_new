de7c49717768d782e474196fab3c9e79ede86bb1
120649969 <120649969@qq.com>
2024-05-30 19:44:57 +0800

    测试模式

--- assets/src/views/adventure/sixModes/WordExamView.ts
+++ assets/src/views/adventure/sixModes/WordExamView.ts
@@ -1,14 +1,35 @@
 import { _decorator, Component, Node } from 'cc';
 import { BaseModeView } from './BaseModeView';
+import List from '../../../util/list/List';
+import { UnitWordModel } from '../../../models/TextbookModel';
+import { GameMode } from '../../../models/AdventureModel';
 const { ccclass, property } = _decorator;
 
 @ccclass('WordExamView')
 export class WordExamView extends BaseModeView {
-    start() {
+    @property({ type: List, tooltip: '选项Item' })
+    itemList: List = null;
+
+    async initData(wordsdata: UnitWordModel[], levelData: any) {
+        this.gameMode = GameMode.Exam;
+        super.initData(wordsdata, levelData);
+        this.initWords(wordsdata);
+        this.initEvent();
+        this.initMonster(); //初始化怪物
+    }
+
+    //获取关卡单词回包
+    initWords(data: UnitWordModel[]) {
+        this._wordsData = data;
+        this.showCurrentWord();
+    }
+
+
+    showCurrentWord() {
 
     }
 
-    update(deltaTime: number) {
+    onItemRender(item: Node, index: number) {
 
     }
 }
---
+++ assets/src/views/adventure/sixModes/items/ExamItem.ts
@@ -0,0 +1,27 @@
+import { _decorator, Component, Label, Node, Sprite } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ExamItem')
+export class ExamItem extends Component {
+    @property({ type: Node, tooltip: "选中背景" })
+    selectBg: Node = null;
+    @property({ type: Label, tooltip: "显示内容" })
+    letterLabel: Label = null;
+    private _select: boolean = false;
+
+    setData(letter: string) {
+        this.letterLabel.string = letter;
+        this.select = false;
+    }
+
+    public get select(): boolean {
+        return this._select;
+    }
+
+    public set select(value: boolean) {
+        this._select = value;
+        this.selectBg.active = value;
+    }
+}
+
+
