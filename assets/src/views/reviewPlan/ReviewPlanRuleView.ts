4c545c50edc7596f9cdedcb48f94828751b3b6c6
ZhouKJ <zh2898211@qq.com>
2024-08-06 17:04:09 +0800

    适配调整

--- assets/src/views/reviewPlan/ReviewPlanRuleView.ts
+++ assets/src/views/reviewPlan/ReviewPlanRuleView.ts
@@ -1,5 +1,7 @@
 import { _decorator, Component, Label, Node } from 'cc';
+import GlobalConfig from '../../GlobalConfig';
 import CCUtil from '../../util/CCUtil';
+import { ToolUtil } from '../../util/ToolUtil';
 import { ReviewSourceType } from './ReviewWordListView';
 const { ccclass, property } = _decorator;
 
@@ -9,8 +11,12 @@ export class ReviewPlanRuleView extends Component {
     public btnClose: Node = null;//关闭按钮
     @property(Label)
     public labelRule: Label = null;//规则内容
+    @property(Node)
+    public frame: Node = null;//背景框
     protected onLoad(): void {
         this.initEvent();
+        let scale = ToolUtil.getValue(GlobalConfig.WIN_DESIGN_RATE, 0.1, 1.0);
+        CCUtil.setNodeScale(this.frame, scale);
     }
     protected onDestroy(): void {
         CCUtil.offTouch(this.btnClose, this.onBtnCloseClick, this);
